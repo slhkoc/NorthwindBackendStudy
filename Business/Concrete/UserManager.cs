59b8ed00d35bd55babbe750380584fdb0fa0d0fa
salih <salihkoc3469@gmail.com>
2021-03-07 17:27:51 +0300

    Rol Yönetimi Data Access Konfigurasyonu ve Business Konfigurasyonu

---
+++ Business/Concrete/UserManager.cs
@@ -0,0 +1,32 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class UserManager : IUserService
+    {
+        IUserDal _userDal;
+        public UserManager(IUserDal userDal)
+        {
+            _userDal = userDal;
+        }
+        public void Add(User user)
+        {
+            _userDal.Add(user);
+        }
+
+        public User GetByMail(string mail)
+        {
+            return _userDal.Get(u => u.Email == mail);
+        }
+
+        public List<OperationClaim> GetClaims(User user)
+        {
+            return _userDal.GetClaims(user);
+        }
+    }
+}
